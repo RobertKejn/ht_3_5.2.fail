b4527bd3c2dbf9faa8776a62475cd0f4f149d828
Raymond <robertkejn@gmail,com>
2023-03-15 00:47:27 +0400

    Initial Commit

---
+++ ht_3_5.3.fail/ht_3_5.2.fail.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+template<class T>
+class Column {
+private:
+	int length = 0;
+	T* arr;
+public:
+	Column() {}
+	Column(const int n) : length(n) {
+		arr = new T[n];
+	}
+	~Column() {
+		delete[] arr;
+	}
+
+	T operator[](int n) const {
+		return arr[n];
+	}
+
+	T operator[](int n) {
+		return arr[n];
+	}
+
+	Column<T>& operator=(const Column<T>& c) {
+		delete[] arr;
+		arr = new T[c.length];
+		length = c.length;
+		return *this;
+	}
+};
+
+template<class T>
+class Table {
+private:
+	int rows = 0;
+	int cols = 0;
+	Column<T>* table;
+public:
+
+	Table(const int rows, const int cols) {
+		table = new Column<T>[rows]();
+		for (int i = 0; i < rows; ++i) {
+			table[i] = Column<T>(cols);
+		}
+		this->rows = rows;
+		this->cols = cols;
+	}
+	~Table() {
+		delete[] table;
+	}
+
+	Table<T>& operator=(Table<T>& t) {
+
+	}
+
+	Column<T> operator[](int i) const {
+		return table[i];
+	}
+
+	Column<T> operator[](int i) {
+		return table[i];
+	}
+
+};
+
+int main()
+{
+	auto table = Table<int>(2, 3);
+	//table[0][0] = 0;
+	std::cout << table[0][0];
+}
